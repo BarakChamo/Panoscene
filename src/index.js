e2bd6e146d05e3eb2ca0616c11fd2c82f2e5ee3a
Barak Chamo <barakchamo@BARAK_CHAMO.ITP.TSOA.NYU.EDU>
2018-04-11 11:42:43 -0400

    clean up sphere positions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'aframe'
 import 'aframe-animation-component'
 import 'aframe-particle-system-component'
 import 'aframe-text-geometry-component'
+import 'aframe-glow'
 import 'babel-polyfill'
 import { Entity, Scene } from 'aframe-react'
 import React from 'react'
@@ -30,6 +31,7 @@ class App extends React.Component {
 
     return (
     <Entity
+      key={i}
       rotation={`0 ${rotation} 0`}
       events={{
         click: this.onClick,
@@ -41,15 +43,15 @@ class App extends React.Component {
         id={`sphere${i}`}
         src={`#v${i}`}
         opacity="0.6"
-        material="metalness: 0.3;"
-        animation__rotate="property: rotation; dur: 10000; loop: true; to: 0 360 0; easing: linear;"
-        position="0 1.5 -5"
+        material="shader: flat;"
+        animation__rotate="property: rotation; dur: 15000; loop: true; to: 0 360 0; easing: linear;"
       >
         { animate && (
-          [<a-animation attribute="material.opacity" from="0.6" to="1.0" />, <a-animation attribute="position" from="0 1.5 -5" to="0 1.5 -3.5"/>]
+          [
+            <a-animation key="opin" attribute="material.opacity" dur="300" from="0.6" to="0.9" />, <a-animation key="pin" attribute="position" from="0 1.5 -5" to="0 1.5 -3.5"  dur="300"/>]
         )}
         {!animate && (
-          [<a-animation attribute="material.opacity" from="1.0" to="0.6"/>, <a-animation attribute="position" from="0 1.5 -3.5" to="0 1.5 -5" />]
+          [<a-animation key="opout" attribute="material.opacity" dur="300" from="0.9" to="0.6"/>, <a-animation key="pout" attribute="position" from="0 1.5 -3.5" to="0 1.5 -5"   dur="300"/>]
         )}
       </a-sphere>
     </Entity>
@@ -65,8 +67,6 @@ class App extends React.Component {
           <video id="v4" src="/4.mp4" loop autoPlay muted={this.state.videoId === 'v4'} />
           <video id="v5" src="/5.mp4" loop autoPlay muted={this.state.videoId === 'v5'} />
 
-          <a-mixin id="unfade" attribute="material.opacity" from="0.6" to="1.0"></a-mixin>
-          <a-mixin id="fade" attribute="material.opacity" from="1.0" to="0.6"></a-mixin>
           <a-asset-item id="font" src="/typeface.json"></a-asset-item>
         </a-assets>
 
@@ -75,13 +75,14 @@ class App extends React.Component {
 
         <a-videosphere src={this.state.videoId} />
 
-        <a-entity position="-0.8 2 -1" text-geometry="font: #font; value: Choose a Panoscene; bevelEnabled: true; bevelSize: 0.005; bevelThickness: 0.005; curveSegments: 12; size: 0.15; height: 0;" material="color:white; metalness:0.5; roughness: 0;" opacity="0.66" scale="" visible=""></a-entity>
+        <a-entity position="-0.8 2.1 -1.5" text-geometry="font: #font; value: Choose a Panoscene; bevelEnabled: true; bevelSize: 0.005; bevelThickness: 0.005; curveSegments: 12; size: 0.15; height: 0;" material="color:white; metalness:0.5; roughness: 0;" opacity="0.66" scale="" visible=""/>
 
         {this.renderSphere(1, 0)}
         {this.renderSphere(2, -30)}
         {this.renderSphere(3, -60)}
         {this.renderSphere(4, 30)}
         {this.renderSphere(5, 60)}
+
 
         <Entity primitive="a-camera">
           <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
